2de0e984e2003cf7c5da58e1fc5285c787e3e70c
Irasov <srnan@yandex.ru>
2015-06-20 14:14:47 +0600

    replacing  List<Word> to  List<String>

--- src/main/java/com/epam/irasov/parser/runner/Runner.java
+++ src/main/java/com/epam/irasov/parser/runner/Runner.java
@@ -2,7 +2,7 @@ package com.epam.irasov.parser.runner;
 
 import com.epam.irasov.parser.entity.*;
 import com.epam.irasov.parser.logic.ParserText;
-import com.epam.irasov.parser.logic.SearchWord;
+import com.epam.irasov.parser.logic.SearchWords;
 import com.epam.irasov.parser.reporter.Reporter;
 import com.epam.irasov.parser.utils.FileOperation;
 
@@ -11,6 +11,6 @@ public class Runner {
         ParserText parser = new ParserText();
         Text text = parser.parse(FileOperation.fileRead());
         System.out.println(text.buildOriginalString());
-        Reporter.printFoundWords(SearchWord.foundWords(text.getPart()));
+        Reporter.printFoundWords(SearchWords.foundWords(text.getPart()));
     }
 }
