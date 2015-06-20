0f3a57bd79101c7c72c09064f2c4e34e07d66f9b
Irasov <srnan@yandex.ru>
2015-06-20 16:10:52 +0600

    char

--- src/main/java/com/epam/irasov/parser/entity/Symbol.java
+++ src/main/java/com/epam/irasov/parser/entity/Symbol.java
@@ -5,11 +5,11 @@ public class Symbol implements  SentencePart {
     public static enum SymbolType{LETTER,PUNCTUATION,WHITESPACE}
 
     private SentencePartType type;
-    private String symbol;
+    private char symbol;
     private SymbolType symbolType;
 
     public Symbol(String symbol, SentencePartType type, SymbolType symbolType ) {
-        this.symbol = symbol;
+        this.symbol = symbol.charAt(0);
         this.type = type;
         this.symbolType = symbolType;
     }
@@ -21,7 +21,7 @@ public class Symbol implements  SentencePart {
     public SymbolType getSymbolType(){return this.symbolType;}
 
     public String buildOriginalString() {
-        return this.symbol;
+        return String.valueOf(this.symbol);
     }
 
     @Override
@@ -29,14 +29,17 @@ public class Symbol implements  SentencePart {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Symbol symbol1 = (Symbol) o;
+        if (symbol != symbol1.symbol) return false;
         if (type != symbol1.type) return false;
-        return !(symbol != null ? !symbol.equals(symbol1.symbol) : symbol1.symbol != null);
+        return symbolType == symbol1.symbolType;
+
     }
 
     @Override
     public int hashCode() {
         int result = type != null ? type.hashCode() : 0;
-        result = 31 * result + (symbol != null ? symbol.hashCode() : 0);
+        result = 31 * result + (int) symbol;
+        result = 31 * result + (symbolType != null ? symbolType.hashCode() : 0);
         return result;
     }
 }
