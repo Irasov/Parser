049a65f9a9b9eaa056a5c68d80e376048a052892
Irasov <srnan@yandex.ru>
2015-06-20 14:14:37 +0600

    replacing  List<Word> to  List<String>

--- src/main/java/com/epam/irasov/parser/reporter/Reporter.java
+++ src/main/java/com/epam/irasov/parser/reporter/Reporter.java
@@ -1,13 +1,15 @@
 package com.epam.irasov.parser.reporter;
 
+import com.epam.irasov.parser.entity.SentencePart;
+
 import java.util.List;
 
 public class Reporter {
-    public static void printFoundWords(List<String> foundWords){
+    public static void printFoundWords(List<SentencePart> foundWords){
         StringBuilder sb = new StringBuilder();
         if(!foundWords.isEmpty()) {
-            for (String word : foundWords) {
-                sb.append(word).append("\n");
+            for (SentencePart word : foundWords) {
+                sb.append(word.buildOriginalString()).append("\n");
             }
         }
         System.out.println(sb.toString());
