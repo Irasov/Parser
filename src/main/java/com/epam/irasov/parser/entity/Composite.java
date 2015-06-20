e613eedcdc009d38bc9c03bad9fe94cd03531254
Irasov <srnan@yandex.ru>
2015-06-20 15:47:36 +0600

    rename

--- src/main/java/com/epam/irasov/parser/entity/Composite.java
+++ src/main/java/com/epam/irasov/parser/entity/Composite.java
@@ -5,7 +5,6 @@ import java.util.List;
 public interface Composite<E extends Component> extends Component {
     void add(E e);
     /*
-    E get(int index);
     E remove(int index);
     E remove(E e);
     boolean contains(E e);
