bc9923577f01f765178c50a402ed1bde1436e855
Irasov <srnan@yandex.ru>
2015-06-21 16:19:08 +0600

    log

--- src/main/java/com/epam/irasov/parser/runner/Runner.java
+++ src/main/java/com/epam/irasov/parser/runner/Runner.java
@@ -5,12 +5,15 @@ import com.epam.irasov.parser.logic.Parser;
 import com.epam.irasov.parser.logic.WordsSearch;
 import com.epam.irasov.parser.reporter.Reporter;
 import com.epam.irasov.parser.utils.FileOperation;
+import org.apache.log4j.Logger;
 
 public class Runner {
+    private static Logger LOGGER = Logger.getLogger(Runner.class);
     public static void main(String[] args) {
+        LOGGER.info("Info: parser project launched");
         Parser parser = new Parser();
         Text text = parser.textParsing(FileOperation.fileRead());
-        System.out.println(text.buildOriginalString());
         Reporter.printFoundWords(WordsSearch.UniqueWordsSearch(WordsSearch.sentenceWords(text.getPart(), 0, 0), WordsSearch.sentenceWordsNotConsiderOnSentence(text.getPart(), 0, 0)));
+        LOGGER.info("Info: parser has completed execution of the project");
     }
 }
