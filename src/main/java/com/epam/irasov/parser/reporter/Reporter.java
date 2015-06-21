84afcfbce4c382dc0ec4522b3f528636c5e89fe1
Irasov <srnan@yandex.ru>
2015-06-21 13:36:15 +0600

    write

--- src/main/java/com/epam/irasov/parser/reporter/Reporter.java
+++ src/main/java/com/epam/irasov/parser/reporter/Reporter.java
@@ -1,17 +1,25 @@
 package com.epam.irasov.parser.reporter;
 
 import com.epam.irasov.parser.entity.SentencePart;
+import com.epam.irasov.parser.utils.FileOperation;
+import com.epam.irasov.parser.utils.Settings;
 
+import java.io.File;
 import java.util.List;
 
 public class Reporter {
     public static void printFoundWords(List<SentencePart> foundWords){
+        Settings settings = Settings.getInstance();
         StringBuilder sb = new StringBuilder();
         if(!foundWords.isEmpty()) {
             for (SentencePart word : foundWords) {
                 sb.append(word.buildOriginalString()).append("\n");
             }
+          //  LOGGER.info("The search words are printed to a fondWords.txt");
+            FileOperation.fileWrite(sb.toString(), settings.getProperties("file.found.words.path"));
         }
-        System.out.println(sb.toString());
+       // else{
+         //   LOGGER.info("Not found words");
+    //    }
     }
 }
