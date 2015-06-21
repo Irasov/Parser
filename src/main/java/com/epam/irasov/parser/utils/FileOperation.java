a0ce249cc051264cb236b63e538c256832e7ee10
Irasov <srnan@yandex.ru>
2015-06-21 12:39:26 +0600

    load settings project

--- src/main/java/com/epam/irasov/parser/utils/FileOperation.java
+++ src/main/java/com/epam/irasov/parser/utils/FileOperation.java
@@ -1,12 +1,19 @@
 package com.epam.irasov.parser.utils;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class FileOperation {
     public static String fileRead() {
+        Settings settings = Settings.getInstance();
+        try {
+            settings.loadSettings();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         StringBuilder text = new StringBuilder();
-        InputStream in = FileOperation.class.getClassLoader().getResourceAsStream("page.txt");
+        InputStream in = FileOperation.class.getClassLoader().getResourceAsStream(settings.getProperties("file.input.path"));
         Scanner scanner = new Scanner(in, "UTF-8");
         scanner.useDelimiter("Z");
         text.append(scanner.next());
